ab51c38b9f82bc88b7d7fc9040a07dbd117d15eb
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-10-12 12:52:02 -0400

    PHP

---
+++ Lab04/myfirstphp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Into</title>
+  </head>
+  <body>
+    <?php
+      echo("<input id='toOutput' />");
+     ?>
+    <button id ="ChangeStuff"></button>
+    <div id="outputDIV">
+
+    </div>
+
+  </body
+</html>
