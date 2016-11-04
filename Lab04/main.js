2b699c9b0c9b381f34927f34ae43d4e191ad5e26
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-04 09:50:56 -0400

    bump

--- Lab04/main.js
+++ Lab04/main.js
@@ -1,35 +1,35 @@
-var input  = document.getElementById('Ghost-Play');
+var input = document.getElementById('Ghost-Play');
 var Player = 0;
 var Playertext = document.getElementById('Player');
 var Losingtext = document.getElementById('Lose-T');
 var numw;
-Playertext.value = "Player "+ (Player+1)+" turn";
-function Check(){
-  var currword = input.value;
-  //console.log(currword);
-  var xhttp = new XMLHttpRequest();
+Playertext.value = "Player " + (Player + 1) + " turn";
+
+function Check() {
+    var currword = input.value;
+    //console.log(currword);
+    var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          numw = this.responseText;
-          //console.log(numw);
-        }}
-        xhttp.open("GET", "https://vtandale-testapp.herokuapp.com/Lab04/Ghost.php?query=" + currword, true);
-        xhttp.send();
-  console.log(numw);
-  console.log(parseInt(numw));
-  console.log(numw==="0");
-  if(numw<1){
-    console.log("L's");
-    Losingtext.value = "Thats not a word Player "+(Player+1)+" loses. The Game Restarts";
-    input.value = "";
-    input.textContent = "";
-    Player = 0;
-  }
-  else{
-  Player = 1-Player;
-}
-  Playertext.value = "Player "+ (Player+1)+" turn";
-  Playertext.textContent = "Player "+ (Player+1)+" turn";
+            numw = this.responseText;
+            console.log(numw);
+            console.log(parseInt(numw));
+            console.log(numw === "0");
+            if (numw < 1) {
+                console.log("L's");
+                Losingtext.value = "Thats not a word Player " + (Player + 1) + " loses. The Game Restarts";
+                input.value = "";
+                input.textContent = "";
+                Player = 0;
+            } else {
+                Player = 1 - Player;
+            }
+            Playertext.value = "Player " + (Player + 1) + " turn";
+            Playertext.textContent = "Player " + (Player + 1) + " turn";
+        }
+    }
+    xhttp.open("GET", "https://vtandale-testapp.herokuapp.com/Lab04/Ghost.php?query=" + currword, true);
+    xhttp.send();
 
 }
 input.addEventListener("keyup", Check, false);
