95ce9a3419b57e7b945148aa8bc740dfc2074bc7
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-04 09:43:35 -0400

    bump

--- Lab04/main.js
+++ Lab04/main.js
@@ -15,7 +15,7 @@ function Check(){
         }}
         xhttp.open("GET", "https://vtandale-testapp.herokuapp.com/Lab04/Ghost.php?query=" + currword, true);
         xhttp.send();
-  console.log(numw==='0');
+  console.log(numw.equals('0'));
   if(numw===0){
     Losingtext.value = "Thats not a word Player "+(Player+1)+" loses. The Game Restarts";
     input.value = "";
