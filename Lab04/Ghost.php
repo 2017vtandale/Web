4588020e4271cf310e534d63086ccc44a2786b51
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-04 13:05:16 -0400

    bump

--- Lab04/Ghost.php
+++ Lab04/Ghost.php
@@ -4,10 +4,12 @@ $txtfile = file_get_contents('words.txt');
 $input = htmlspecialchars($_GET["query"]);
 // $input  = readline("Type Word: " );
 $regex = "/\n".$input."/";
-$regex2 = "/\s".$input."/";
+$regex2 = "/\s".$input."\s/";
 $check = preg_match_all($regex,$txtfile,$match);
-$check1 = preg_match_all($regex2,$txtfile,$match);
+$check1 = preg_match($regex2,$txtfile,$match);
 // if($check1>0&&strlen($input)>3)
 //   $check = 0;
+if($check1)
+  $check = 0;
 echo ($check."\n");
 ?>
