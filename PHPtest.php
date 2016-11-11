3f67fa9f73c26edb2fde80d7a1e1e8bf3adddc4a
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-11 13:28:28 -0500

    bump

--- PHPtest.php
+++ PHPtest.php
@@ -1,12 +1,12 @@
 <?php
-  $host ="host=ec2-184-72-240-189.compute-1.amazonaws.com";
-  $dbname="dbname=dcneg2tjgbcblv";
-  $user ="user=qpqqhnjarylryo";
+  $host ="host=ec2-23-23-225-246.compute-1.amazonaws.com";
+  $dbname="dbname=d2g9hb311v8dl8";
+  $user ="user=puuhajyxwxnpbc";
   $port ="port-5432";
-  $password ="password=hdGBdKUpo5ei2l5OGk-n0iFnMn";
+  $password ="password=qX9Xe_KHWJG_AmTzjoUUbNwDQ6";
   $db = pg_connect($host." ".$dbname." ".$user." ".$port." ".$password);
 
-  $query= "  CREATE TABLE football(TeamName varchar(255),NumberOfWins int)";
+  $query= "  CREATE TABLE More_Football(TeamName varchar(255),NumberOfWins int)";
   echo $query;
   $ret = pg_query($db,$query);
   if(!$ret)
