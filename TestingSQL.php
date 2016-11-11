f24319a5ee588280bd354cd1a4797a7ce3fc870d
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-11 13:33:38 -0500

    bump

--- TestingSQL.php
+++ TestingSQL.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 	//instantiate our DB object
-	$host="host=ec2-23-23-225-246.compute-1.amazonaws.com";
-	$dbname="dbname=d2g9hb311v8dl8";
-	$user="user=puuhajyxwxnpbc";
-	$port="port=5432";
-	$password="password=qX9Xe_KHwJG_AmTzjoUUbNwDQ6";
+	  $host ="host=ec2-184-72-240-189.compute-1.amazonaws.com";
+	  $dbname="dbname=dcneg2tjgbcblv";
+	  $user ="user=qpqqhnjarylryo";
+	  $port ="port-5432";
+	  $password ="password=hdGBdKUpo5ei2l5OGk-n0iFnMn";
 	$db = pg_connect($host." ".$dbname." ".$user." ".$port." ".$password);
 	//Create a table try/catch
 	$query = <<<ESCAPED
