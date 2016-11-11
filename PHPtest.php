d65ce9e6f3cf70451523d4b09409843dc0b4e492
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-11 13:12:32 -0500

    bump

--- PHPtest.php
+++ PHPtest.php
@@ -1,10 +1,10 @@
 <?php
-  $host ="host = ec2-184-72-240-189.compute-1.amazonaws.com";
-  $dbname="dcneg2tjgbcblv";
-  $user ="user = qpqqhnjarylryo";
-  $port ="port 5432";
-  $password ="password = hdGBdKUpo5ei2l5OGk-n0iFnMn";
-  $db = PE_connect($host." ".$dbname." ".$user." ".$port." ".$password);
+  $host ="host=ec2-184-72-240-189.compute-1.amazonaws.com";
+  $dbname="dbname=dcneg2tjgbcblv";
+  $user ="user=qpqqhnjarylryo";
+  $port ="port-5432";
+  $password ="password=hdGBdKUpo5ei2l5OGk-n0iFnMn";
+  $db = pg_connect($host." ".$dbname." ".$user." ".$port." ".$password);
 
   $query= <<<ESCAPED
     CREATE TABLE football
@@ -16,11 +16,11 @@
 ESCAPED;
   $ret = pg_query($query);
   if(!$ret)
-  (
+  {
     echo(pg_last_error($db));
-  )
+  }
   else
-  (
+  {
   echo "it worked!!!";
-  )
+}
 ?>
