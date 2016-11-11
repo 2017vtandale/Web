641c0b8700b26abd11277c37ce55f2a34e3f0a49
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-11 13:41:05 -0500

    bump

--- TestingSQL.php
+++ TestingSQL.php
@@ -1,10 +1,10 @@
 <?php
 	//instantiate our DB object
-	  $host ="host=ec2-184-72-240-189.compute-1.amazonaws.com";
-	  $dbname="dbname=dcneg2tjgbcblv";
-	  $user ="user=qpqqhnjarylryo";
-	  $port ="port-5432";
-	  $password ="password=hdGBdKUpo5ei2l5OGk-n0iFnMn";
+	$host ="host=ec2-184-72-240-189.compute-1.amazonaws.com";
+	$dbname="dbname=dcneg2tjgbcblv";
+	$user ="user=qpqqhnjarylryo";
+	$port ="port-5432";
+	$password ="password=hdGBdKUpo5ei2l5OGk-n0iFnMn";
 	$db = pg_connect($host." ".$dbname." ".$user." ".$port." ".$password);
 	//Create a table try/catch
 	$query = <<<ESCAPED
@@ -26,7 +26,7 @@ ESCAPED;
 	//Add some data
 	$query = <<<ESCAPED
 		INSERT INTO Football VALUES(
-			"Redskins", 4
+			Redskins, 4
 		);
 ESCAPED;
 $ret = pg_query($query);
