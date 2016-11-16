2e5ecd1c9065a85d5423ce49bbb4fc2f0f0e2653
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-16 09:03:46 -0500

    bump

--- Lab5/insert.php
+++ Lab5/insert.php
@@ -1,18 +1,11 @@
 <?php
 $connectstr =  "dbname=d2p8qh6esl50d9 host=ec2-54-243-59-15.compute-1.amazonaws.com port=5432 user=umytqcshywmkvl password=-suxmkeL_GhbeXFKIlKzbxr1Kp sslmode=require";
 $db = pg_connect($connectstr);
-
-$query= <<<ESCAPED
-       CREATE TABLE Tacobell(
-          Itemname varchar(255), Price float
-       )
-ESCAPED;
-
-$ret = pg_query($query);
-if(!$ret){
-echo(pg_last_error($db));
-}
-else{
-echo("successfully added row");
+$foodnames = array("Chicken Soft Taco","Chicken Hard Taco","Cheesy Gordita Crunch","XXL Grilled Stuffed Burrito","Smothered Burrito","Shredded Chicken Burrito","Crunch Wrap Supreme","Grand Scrambler");
+$prices = array("1.79","1.19","2.89","4.19","4.19","1.89","3.19","4.69");
+for( $i=0;   $i<8;   $i++ ){
+       $query = "INSERT INTO tacobell VALUES($foodnames[$i], $prices[$i])";
+       $ret = pg_query($query);
+       //echo($foodnames[$i].$prices[$i]);
 }
 ?>
--- TestingSQL.php
+++ TestingSQL.php
@@ -3,23 +3,35 @@
 	$host ="host=ec2-54-243-59-15.compute-1.amazonaws.com";
 	$dbname="dbname=d2p8qh6esl50d9";
 	$user ="user=umytqcshywmkvl";
-	$port ="port=5432";
+	$port ="port-5432";
 	$password ="password=-suxmkeL_GhbeXFKIlKzbxr1Kp";
 	$connectstr =  "dbname=d2p8qh6esl50d9 host=ec2-54-243-59-15.compute-1.amazonaws.com port=5432 user=umytqcshywmkvl password=-suxmkeL_GhbeXFKIlKzbxr1Kp sslmode=require";
 	$db = pg_connect($connectstr);
-
-
-$query = "SELECT * FROM Football";
+	//Create a table try/catch
+	echo($db);
+	$query = "CREATE TABLE Football(TeamName varchar(255),NumberOfWins int)";
 	$ret = pg_query($query);
+
 	if(!$ret){
-			echo(pg_last_error($db));
+		echo(pg_last_error($db));
 	}
 	else{
-			while ($row = pg_fetch_row($ret)) {
-					echo("Team:".$row[0]."Num of Wins:".$row[1]);
-					//echo "<br />\n";
-			}
-			echo("success");
+		echo "It worked!!!!";
+	}
+	//Add some data
+	$query = <<<ESCAPED
+		INSERT INTO Football VALUES(
+			Redskins, 4
+		);
+ESCAPED;
+$ret = pg_query($query);
+	if(!$ret)
+	{
+		echo(pg_last_error($db));
+	}
+	else
+	{
+		echo "It worked!!!!";
 	}
 	//Query our table
 ?>
