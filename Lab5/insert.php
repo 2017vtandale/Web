bb7840c140f7dab295bac4275edb584b642ab5c4
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-15 22:48:32 -0500

    bump

---
+++ Lab5/insert.php
@@ -0,0 +1,18 @@
+<?php
+$connectstr =  "dbname=d2p8qh6esl50d9 host=ec2-54-243-59-15.compute-1.amazonaws.com port=5432 user=umytqcshywmkvl password=-suxmkeL_GhbeXFKIlKzbxr1Kp sslmode=require";
+$db = pg_connect($connectstr);
+
+$query= <<<ESCAPED
+       CREATE TABLE Tacobell(
+          Itemname varchar(255), Price double
+       )
+ESCAPED;
+
+$ret = pg_query($query);
+if(!$ret){
+echo(pg_last_error($db));
+}
+else{
+echo("successfully added row");
+}
+?>
--- TestingSQL.php
+++ TestingSQL.php
@@ -3,7 +3,7 @@
 	$host ="host=ec2-54-243-59-15.compute-1.amazonaws.com";
 	$dbname="dbname=d2p8qh6esl50d9";
 	$user ="user=umytqcshywmkvl";
-	$port ="port-5432";
+	$port ="port=5432";
 	$password ="password=-suxmkeL_GhbeXFKIlKzbxr1Kp";
 	$connectstr =  "dbname=d2p8qh6esl50d9 host=ec2-54-243-59-15.compute-1.amazonaws.com port=5432 user=umytqcshywmkvl password=-suxmkeL_GhbeXFKIlKzbxr1Kp sslmode=require";
 	$db = pg_connect($connectstr);
