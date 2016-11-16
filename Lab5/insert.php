7c6e2e34aee5a3140aef5c295eabc9b355230cba
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-16 12:42:01 -0500

    bump

--- Lab5/insert.php
+++ Lab5/insert.php
@@ -4,7 +4,7 @@ $db = pg_connect($connectstr,PGSQL_CONNECT_FORCE_NEW);
 $foodnames = array('Chicken Soft Taco','Chicken Hard Taco','Cheesy Gordita Crunch','XXL Grilled Stuffed Burrito','Smothered Burrito','Shredded Chicken Burrito','Crunch Wrap Supreme','Grand Scrambler');
 $prices = array('1.79','1.19','2.89','4.19','4.19','1.89','3.19','4.69');
 for( $i=0;   $i<8;   $i++ ){
-       $query = "INSERT INTO tacobell (itemname, price) VALUES($foodnames[$i], $prices[$i])";
+       $query = "INSERT INTO tacobell (itemname, price) VALUES(".$foodnames[$i].", ".$prices[$i].")";
        $ret = pg_query($query);
        if(!$ret){
        echo(pg_last_error($db));
