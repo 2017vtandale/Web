d36f2a3dddb343c0adbfdccfcde210429ec892d8
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-15 22:21:35 -0500

    bump

--- TestingSQL.php
+++ TestingSQL.php
@@ -5,7 +5,8 @@
 	$user ="user=umytqcshywmkvl";
 	$port ="port-5432";
 	$password ="password=-suxmkeL_GhbeXFKIlKzbxr1Kp";
-	$db = pg_connect($host." ".$dbname." ".$user." ".$port." ".$password,PGSQL_CONNECT_FORCE_NEW);
+	$connectstr =  "dbname=d2p8qh6esl50d9 host=ec2-54-243-59-15.compute-1.amazonaws.com port=5432 user=umytqcshywmkvl password=-suxmkeL_GhbeXFKIlKzbxr1Kp sslmode=require";
+	$db = pg_connect($connectstr,PGSQL_CONNECT_FORCE_NEW);
 	//Create a table try/catch
 	echo($db);
 	$query = "CREATE TABLE Football(TeamName varchar(255),NumberOfWins int)";
