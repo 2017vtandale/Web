9735073b27fff1a90128ed765d932dc749058a76
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-11-16 21:33:30 -0500

    bump

--- Lab5/main.js
+++ Lab5/main.js
@@ -1,5 +1,5 @@
 var showbut = document.getElementById('shownew');
-showbut.onclick = function(){
+showbut.addEventListener("click", function(){
   var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -8,4 +8,4 @@ showbut.onclick = function(){
     }
     xhttp.open("GET", "https://vtandale-testapp.herokuapp.com/Lab5/shownew.php", true);
     xhttp.send();
-};
+},false);
